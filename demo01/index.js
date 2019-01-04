ed5b0c931252faa2e71c9d924c80b26d0b82f6c4
1406081008@qq.com <cjt19930809>
2019-01-04 10:25:55 +0800

    demo01新增store描绘边框的样例

--- demo01/index.js
+++ demo01/index.js
@@ -7,4 +7,18 @@ window.addEventListener('DOMContentLoaded', () => {
     ctx.fillRect(50, 50, 50, 50);
     ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
     ctx.fillRect(75, 75, 50, 50);
+
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = 15; // 设置线的宽度
+    ctx.strokeRect(75, 75, 50, 50);
+
+
+    ctx.fillStyle = 'rgb(140, 150, 80, 0.8)';
+    ctx.fillRect(200, 200, 400, 200);
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth = 2;
+    ctx.strokeRect(280, 230, 240, 140);
+
+    // 清空矩形中的一些区域
+    ctx.clearRect(300, 250, 200, 100);
 })
