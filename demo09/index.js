e551dd8b1c599698c5dd3813affa357ef29a4c2c
1406081008@qq.com <cjt19930809>
2019-01-06 19:13:56 +0800

    新增demo09，用以演示图像变换和状态保存、回滚例子

---
+++ demo09/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const $draw = document.querySelector('#draw');
+    const ctx = $draw.getContext('2d');
+    const $html = document.documentElement;
+    $draw.width = $html.clientWidth;
+    $draw.height = $html.clientHeight;
+
+    ctx.lineWidth = 5;
+    ctx.fillStyle = 'red';
+    ctx.save();
+
+    // 将绘制参考坐标点移动至(400, 200)
+    ctx.translate(400, 200);
+    // 填充色修改为蓝色
+    ctx.fillStyle = 'blue';
+    // 将基于偏移后的参考坐标，进行移动，点为(500, 300)
+    ctx.fillRect(100, 100, 100, 100);
+
+    // 回滚状态
+    ctx.restore();
+    // 绘制一个基于（0，0）原点做偏移的矩形，填充色为红色
+    ctx.fillRect(100, 100, 100, 100);
+
+});
